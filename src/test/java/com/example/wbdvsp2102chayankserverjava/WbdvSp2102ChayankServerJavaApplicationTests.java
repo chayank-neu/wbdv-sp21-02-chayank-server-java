49b22c5739336e7d29be28d79dd36286d4bb3e07
Chayank11 <chayank.tm@gmail.com>
2021-01-21 19:23:15 -0500

    Created login web page

---
+++ src/test/java/com/example/wbdvsp2102chayankserverjava/WbdvSp2102ChayankServerJavaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.wbdvsp2102chayankserverjava;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WbdvSp2102ChayankServerJavaApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
